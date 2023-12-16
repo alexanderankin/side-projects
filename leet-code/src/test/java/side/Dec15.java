443660af9228eedf74ab3f527fdae100b4438680
David Ankin <daveankin@gmail.com>
2023-12-15 19:45:19 -0500

    start with leetcode folder

---
+++ leet-code/src/test/java/side/Dec15.java
@@ -0,0 +1,11 @@
+package side;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NewClassNamingConvention")
+class Dec15 {
+    @Test
+    void test() {
+        System.out.println("hi");
+    }
+}
