e56e9283a451f9f50c9cb5978313aa6dc78af640
David Ankin <daveankin@gmail.com>
2024-05-07 19:28:10 -0400

    confusion

---
+++ leet-code/leet-code/src/test/java/side/Apr13_2024.java
@@ -0,0 +1,58 @@
+package side;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class Apr13_2024 {
+    @Test
+    void test() {
+        // PriorityQueue<Integer> integers = new PriorityQueue<>(Comparator.comparingInt(i -> -i));
+        // integers.add(20);
+        // integers.add(26);
+        //
+        // System.out.println(integers.peek());
+
+        String instructions = """
+                1 97
+                2
+                1 20
+                2
+                1 26
+                1 20
+                2
+                3
+                1 91
+                3""";
+        var result = getMax(Arrays.asList(instructions.split("\r?\n")));
+        System.out.println(result);
+    }
+
+    public static List<Integer> getMax(List<String> operations) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(i -> -i));
+        PriorityQueue<Integer> priorityQueue1 = new PriorityQueue<>();
+        List<Integer> result = new ArrayList<>();
+        for (String o : operations) {
+            String[] parts = o.split(" ");
+            switch(parts[0]) {
+                case "1": {
+                    priorityQueue.add(Integer.valueOf(parts[1]));
+                    priorityQueue1.add(Integer.valueOf(parts[1]));
+                    break;
+                }
+                case "2": {
+                    priorityQueue.poll();
+                    priorityQueue1.poll();
+                    break;
+                }
+                case "3": {
+                    Integer peek = priorityQueue.peek();
+                    result.add(peek);
+                    break;
+                }
+            }
+        }
+        return result;
+
+    }
+}
