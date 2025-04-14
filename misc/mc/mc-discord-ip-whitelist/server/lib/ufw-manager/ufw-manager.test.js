4ad50a8b1e29432dbfe9436fea439df666eed8a3
David Ankin <daveankin@gmail.com>
2025-04-14 18:33:53 -0400

    record findings

--- misc/mc/mc-discord-ip-whitelist/server/lib/ufw-manager/ufw-manager.test.js
+++ misc/mc/mc-discord-ip-whitelist/server/lib/ufw-manager/ufw-manager.test.js
@@ -3,7 +3,17 @@ import { container } from "../../test/testSetup.js";
 import { UfwManager } from "./ufw-manager.js";
 
 /**
- * AAAAAhhhhhh!!!!!
+ * ok so here is where I realized that i actually need the component that does the UFW calls to be separated from the API component with a network boundary
+ * <p>
+ * because the development machine is not guaranteed to be linux, I need to actually talk to it over a network not just do cli calls.
+ * <p>
+ * otherwise all i can do is cli calls, and the part of the testing that is the containerization becomes part of the stack.
+ * <p>
+ * the thing i wanted to do was talk to bare metal in prod, and containers in test
+ * <p>
+ * so i just need to replace this with a network boundary.
+ * <p>
+ * maybe this project requires some better langauge....
  */
 describe("UfwManager", () => {
   describe("initialization", () => {
