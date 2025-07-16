3a8a98f1eb2635aeb71de5278be8eb4aea93c81c
David Ankin <daveankin@gmail.com>
2025-07-16 18:59:17 -0400

    test user data plain_text_passwd

--- cloud/vm/qemu-cloud-init/create-user-data.sh
+++ cloud/vm/qemu-cloud-init/create-user-data.sh
@@ -11,13 +11,15 @@ user:
   groups: sudo, users, admin
   shell: /bin/bash
   lock_passwd: false
-  passwd: "$(mkpasswd ubuntu)"
-  hashed_passwd: "$(mkpasswd ubuntu)"
-  ssh_authorized_keys: $(cat \
+  # passwd: "$(mkpasswd ubuntu)"
+  # hashed_passwd: "$(mkpasswd ubuntu)"
+  # ssh_authorized_keys: $(cat \
     <(cat ~/.ssh/id_rsa.pub) \
     <(curl -fSsL https://github.com/alexanderankin.keys) \
     | sed -e '/^$/d' -e 's/^/"/' -e 's/$/"/' \
     | jq -sc)
+  plain_text_passwd: ubuntu1
+  ssh_authorized_keys: []
 
 users:
   - default
@@ -36,10 +38,10 @@ ssh_pwauth: true
 disable_root: true
 
 #package_update: true
-#packages: [ htop, jq, curl, wget, git, net-tools, gpg ]
+packages: [ htop, jq, curl, wget, git, net-tools, gpg ]
 
 EOF
-#exit 0
+exit 0
 
 cat <<'EOF'
 runcmd:
