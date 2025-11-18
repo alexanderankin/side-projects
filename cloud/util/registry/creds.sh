b4e09abf503f5d3b294fb362663f65d32299f9d0
David Ankin <daveankin@gmail.com>
2025-11-18 03:07:11 -0500

    simple registry for home use

---
+++ cloud/util/registry/creds.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+if [[ "$0" != "$BASH_SOURCE" ]]; then echo "no sourcing">&2; return 1; fi;
+set -eu -o pipefail
+[[ ! -z ${DEBUG:-} ]] && set -x
+full="$(readlink -f "$BASH_SOURCE")"; dir="${full%\/*}"; file="${full##*/}";
+
+cd "${dir}"
+
+username=${1}
+password=${2:-}
+
+if [[ -z "${password}" ]]; then password=$(cat /dev/urandom | tr -dc '#A-Za-z0-9_' | head -c 50 || :;); fi
+
+if [[ -s .htpasswd ]]; then
+  sed -e "/^${username}:/d" -i .htpasswd
+fi
+
+docker run --entrypoint htpasswd httpd:2 -Bbn "${username}" "${password}" > .htpasswd
+
+echo "${password}" > "${username}.cred"
