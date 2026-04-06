1e3b43b7a33dc48f5ffef628cb15d495f56303a6
David Ankin <daveankin@gmail.com>
2026-04-06 16:25:18 -0700

    fix test now it validates something

--- cloud/util/util-acme/acme-lib/src/itest/java/side/cloud/util/acme/lib/challenges/PebbleChallengeSolverITest.java
+++ cloud/util/util-acme/acme-lib/src/itest/java/side/cloud/util/acme/lib/challenges/PebbleChallengeSolverITest.java
@@ -3,14 +3,18 @@ package side.cloud.util.acme.lib.challenges;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.rnorth.ducttape.unreliables.Unreliables;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestClient;
 import side.cloud.util.acme.lib.AcmeClient;
 import side.cloud.util.acme.lib.AcmeClientTemplate;
 import side.cloud.util.acme.lib.AcmeLibBaseITest;
-import side.cloud.util.acme.lib.challenges.pebble.PebbleChallengeServerClient;
 import side.cloud.util.acme.lib.challenges.pebble.PebbleChallengeSolver;
 import side.cloud.util.acme.lib.model.AcmeIdentifier;
-import side.cloud.util.acme.lib.model.AcmeResources.*;
+import side.cloud.util.acme.lib.model.AcmeResources.Authorization;
+import side.cloud.util.acme.lib.model.AcmeResources.Challenge;
+import side.cloud.util.acme.lib.model.AcmeResources.NewAccount;
+import side.cloud.util.acme.lib.model.AcmeResources.NewOrder;
 import side.cloud.util.acme.lib.model.SupportedClientKeyPairAlgorithm;
 
 import java.net.URI;
@@ -18,16 +22,18 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static side.cloud.util.acme.lib.model.AcmeIdentifier.AcmeIdentifierType.dns;
+import static side.cloud.util.acme.lib.model.AcmeResources.Challenge.ChallengeStatus.*;
 
 class PebbleChallengeSolverITest extends AcmeLibBaseITest {
     @SneakyThrows
     @Test
-    void test() {
+    void test_httpChallenge() {
         var sckp = SupportedClientKeyPairAlgorithm.ES256.generate();
         var acmeClient = new AcmeClient(
                 new AcmeClient.Configuration()
@@ -54,15 +60,20 @@ class PebbleChallengeSolverITest extends AcmeLibBaseITest {
         assertThat(c, is(notNullValue()));
 
         var pebbleChallengeClient = getPebbleChallengeClient();
-        // pebbleChallengeClient.setDnsCname("testnonwildcard.example", "pebble-challenge-server");
         var solver = new PebbleChallengeSolver(new PebbleChallengeSolver.Config().setClient(pebbleChallengeClient));
         solver.httpChallenge(c, sckp);
 
-        var challengeReady = acmeClient.postResource(sckp, account.id(), c.getUrl(), Map.of(), String.class);
-        System.out.println("challengeReady: " + challengeReady);
+        assertThat(c.getStatus(), is(pending));
+        acmeClient.postResource(sckp, account.id(), c.getUrl(), Map.of(), Challenge.class);
 
-        Thread.sleep(10_000);
-        var challenge = acmeClient.getResource(sckp, account.id(), c.getUrl(), String.class);
-        System.out.println("challenge: " + challenge);
+        Challenge finalC = c;
+        var challenge = Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
+            var latestChallenge = acmeClient.getResource(sckp, account.id(), finalC.getUrl(), Challenge.class);
+            Assert.isTrue(Set.of(valid, invalid).contains(latestChallenge.getStatus()), "status be terminal");
+            return latestChallenge;
+        });
+
+        assertThat("challenge did not reach terminal status before timeout", challenge, is(notNullValue()));
+        assertThat("challenge should be valid but was " + challenge, challenge.getStatus(), is(equalTo(valid)));
     }
 }
