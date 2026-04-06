eaaeea4af25f43dc72ef8e5c75a6b558762610a5
David Ankin <daveankin@gmail.com>
2026-04-06 16:48:58 -0700

    dns with and without wildcard

--- cloud/util/util-acme/acme-lib/src/itest/java/side/cloud/util/acme/lib/challenges/PebbleChallengeSolverITest.java
+++ cloud/util/util-acme/acme-lib/src/itest/java/side/cloud/util/acme/lib/challenges/PebbleChallengeSolverITest.java
@@ -49,32 +49,29 @@ class PebbleChallengeSolverITest extends AcmeLibBaseITest {
                 .setIdentifiers(List.of(new AcmeIdentifier().setType(dns).setValue("testnonwildcard.example")))
                 .setNotBefore(now)
                 .setNotAfter(now.plus(Duration.ofDays(10))));
-        Challenge c = null;
-        for (var authorization : order.resource().getAuthorizations()) {
-            var resource = acmeClient.getResource(sckp, account.id(), authorization, Authorization.class);
-            System.out.println("resource: " + resource);
-            var challenges = resource.getChallenges();
-            challenges.forEach(System.out::println);
-            c = challenges.stream().filter(cc -> cc.getType().equals("http-01")).findAny().orElseThrow();
-        }
-        assertThat(c, is(notNullValue()));
+
+        assertThat(order.resource().getAuthorizations(), hasSize(1));
+        var authorizationId = order.resource().getAuthorizations().getFirst();
+        var authorization = acmeClient.getResource(sckp, account.id(), authorizationId, Authorization.class);
+        var challenge = authorization.getChallenges().stream()
+                .filter(c -> c.getType().equals("http-01"))
+                .findAny().orElseThrow();
 
         var pebbleChallengeClient = getPebbleChallengeClient();
         var solver = new PebbleChallengeSolver(new PebbleChallengeSolver.Config().setClient(pebbleChallengeClient));
-        solver.httpChallenge(c, sckp);
+        solver.httpChallenge(challenge, sckp);
 
-        assertThat(c.getStatus(), is(pending));
-        acmeClient.postResource(sckp, account.id(), c.getUrl(), Map.of(), Challenge.class);
+        assertThat(challenge.getStatus(), is(pending));
+        acmeClient.postResource(sckp, account.id(), challenge.getUrl(), Map.of(), Challenge.class);
 
-        Challenge finalC = c;
-        var challenge = Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
-            var latestChallenge = acmeClient.getResource(sckp, account.id(), finalC.getUrl(), Challenge.class);
+        var doneChallenge = Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
+            var latestChallenge = acmeClient.getResource(sckp, account.id(), challenge.getUrl(), Challenge.class);
             Assert.isTrue(Set.of(valid, invalid).contains(latestChallenge.getStatus()), "status be terminal");
             return latestChallenge;
         });
 
-        assertThat("challenge did not reach terminal status before timeout", challenge, is(notNullValue()));
-        assertThat("challenge should be valid but was " + challenge, challenge.getStatus(), is(equalTo(valid)));
+        assertThat("doneChallenge did not reach terminal status before timeout", doneChallenge, is(notNullValue()));
+        assertThat("doneChallenge should be valid but was " + doneChallenge, doneChallenge.getStatus(), is(equalTo(valid)));
     }
 
     @SneakyThrows
@@ -90,35 +87,38 @@ class PebbleChallengeSolverITest extends AcmeLibBaseITest {
         var account = acmeClient.newAccount(sckp, new NewAccount()
                 .setContact(List.of(URI.create("mailto:example@example.com")))
                 .setTermsOfServiceAgreed(true));
-        var now = Instant.now();
+
         var dnsHost = "testdnschallenge.localhost";
-        var order = acmeClient.newOrder(sckp, account.id(), new NewOrder()
-                .setIdentifiers(List.of(new AcmeIdentifier().setType(dns).setValue("*." + dnsHost)))
-                .setNotBefore(now)
-                .setNotAfter(now.plus(Duration.ofDays(10))));
+        for (var identifier : List.of("*." + dnsHost, dnsHost)) {
+            var now = Instant.now();
+            var order = acmeClient.newOrder(sckp, account.id(), new NewOrder()
+                    .setIdentifiers(List.of(new AcmeIdentifier().setType(dns).setValue(identifier)))
+                    .setNotBefore(now)
+                    .setNotAfter(now.plus(Duration.ofDays(10))));
 
-        assertThat(order.resource().getAuthorizations(), hasSize(1));
-        var authorizationId = order.resource().getAuthorizations().getFirst();
-        var authorization = acmeClient.getResource(sckp, account.id(), authorizationId, Authorization.class);
-        var challenge = authorization.getChallenges().stream()
-                .filter(c -> c.getType().equals("dns-01"))
-                .findAny().orElseThrow();
+            assertThat(order.resource().getAuthorizations(), hasSize(1));
+            var authorizationId = order.resource().getAuthorizations().getFirst();
+            var authorization = acmeClient.getResource(sckp, account.id(), authorizationId, Authorization.class);
+            var challenge = authorization.getChallenges().stream()
+                    .filter(c -> c.getType().equals("dns-01"))
+                    .findAny().orElseThrow();
 
-        var pebbleChallengeClient = getPebbleChallengeClient();
-        var solver = new PebbleChallengeSolver(new PebbleChallengeSolver.Config()
-                .setClient(pebbleChallengeClient));
-        solver.dnsChallenge(challenge, sckp, authorization);
+            var pebbleChallengeClient = getPebbleChallengeClient();
+            var solver = new PebbleChallengeSolver(new PebbleChallengeSolver.Config()
+                    .setClient(pebbleChallengeClient));
+            solver.dnsChallenge(challenge, sckp, authorization);
 
-        assertThat(challenge.getStatus(), is(pending));
-        acmeClient.postResource(sckp, account.id(), challenge.getUrl(), Map.of(), Challenge.class);
+            assertThat(challenge.getStatus(), is(pending));
+            acmeClient.postResource(sckp, account.id(), challenge.getUrl(), Map.of(), Challenge.class);
 
-        var doneChallenge = Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
-            var latestChallenge = acmeClient.getResource(sckp, account.id(), challenge.getUrl(), Challenge.class);
-            Assert.isTrue(Set.of(valid, invalid).contains(latestChallenge.getStatus()), "status be terminal");
-            return latestChallenge;
-        });
+            var doneChallenge = Unreliables.retryUntilSuccess(10, TimeUnit.SECONDS, () -> {
+                var latestChallenge = acmeClient.getResource(sckp, account.id(), challenge.getUrl(), Challenge.class);
+                Assert.isTrue(Set.of(valid, invalid).contains(latestChallenge.getStatus()), "status be terminal");
+                return latestChallenge;
+            });
 
-        assertThat("doneChallenge did not reach terminal status before timeout", doneChallenge, is(notNullValue()));
-        assertThat("doneChallenge should be valid but was " + doneChallenge, doneChallenge.getStatus(), is(equalTo(valid)));
+            assertThat("doneChallenge did not reach terminal status before timeout", doneChallenge, is(notNullValue()));
+            assertThat("doneChallenge should be valid but was " + doneChallenge, doneChallenge.getStatus(), is(equalTo(valid)));
+        }
     }
 }
