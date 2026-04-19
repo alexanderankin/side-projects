d13fa13508a34dce1450264b35d51aefad58fb9a
David Ankin <daveankin@gmail.com>
2026-04-18 23:27:50 -0400

    browse minecraft jars in intellij

---
+++ misc/mc/mc-server-jar/src/main/java/org/example/Explore.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import net.minecraft.bundler.Main;
+
+public class Explore {
+    static void main() {
+        net.minecraft.bundler.Main main = new Main();
+        net.minecraft.server.Main server;
+    }
+}
